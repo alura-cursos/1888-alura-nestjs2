78274efd535f03cf07a648ff9a95bb9a6d05dce9
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-08-28 18:40:36 -0300

    O que é validação?

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,29 @@
+import { IsNotEmpty, IsString, IsEmail, isEmail } from 'class-validator';
 
 export class Usuario {
     id: number;
+
+    @IsNotEmpty({
+        message: 'nomeDeUsuario é obrigatório.'
+    })
+    @IsString({
+        message: 'nomeDeUsuario precisa ser uma string.'
+    })
     nomeDeUsuario: string;
+
+    @IsEmail({}, {
+        message: 'email precisa ser um endereço de email válido.'
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'senha é obrigatório.'
+    })
     senha: string;
+
+    @IsNotEmpty({
+        message: 'nomeCompleto é obrigatório.'
+    })
     nomeCompleto: string;
     dataDeEntrada: Date;
 }
