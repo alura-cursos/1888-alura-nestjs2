22a489c4cfce02ce1a0462e1a3f860870c3a8f6a
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-09-02 19:29:02 -0300

    O que é serialização?

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,13 @@
 import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
 import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico.validator';
 
 export class Usuario {
     id: number;
 
+    @Expose({
+        name: 'username'
+    })
     @IsNomeDeUsuarioUnico({
         message: 'nomeDeUsuario precisa ser único.'
     })
@@ -15,19 +19,35 @@ export class Usuario {
     })
     nomeDeUsuario: string;
 
+    @Expose({
+        name: 'email'
+    })
     @IsEmail({}, {
         message: 'email precisa ser um endereço de email válido.'
     })
     email: string;
 
+    @Expose({
+        name: 'password'
+    })
+    @Exclude({
+        toPlainOnly: true
+    })
     @IsNotEmpty({
         message: 'senha é obrigatório.'
     })
     senha: string;
 
+    @Expose({
+        name: 'fullName'
+    })
     @IsNotEmpty({
         message: 'nomeCompleto é obrigatório.'
     })
     nomeCompleto: string;
+
+    @Expose({
+        name: 'joinDate'
+    })
     dataDeEntrada: Date;
 }
