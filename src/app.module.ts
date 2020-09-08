e1c9478cca115f862fdc313001e1c87c354e1a2f
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-09-08 14:31:28 -0300

    Tratamento de erros.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module, ClassSerializerInterceptor } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { FitroDeExcecaoHttp } from './common/filtros/filtro-de-excecao-http.filter';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FitroDeExcecaoHttp
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor
