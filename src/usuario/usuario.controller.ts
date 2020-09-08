94be2bac2ec4acacc3c78aad73f26c5c51f08167
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-09-08 17:51:15 -0300

    Cabeçalhos dinâmicos.

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Res, HttpStatus } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { Usuario } from './usuario.entity';
 
@@ -15,9 +15,13 @@ export class UsuarioController {
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario): Usuario {
+    public cria(@Body() usuario: Usuario, @Res() res) {
+        // 'Location', '/users/maria'
         const usuarioCriado = this.usuarioService.cria(usuario);
+        res.status(HttpStatus.CREATED)
+            .location(`/users/${usuarioCriado.nomeDeUsuario}`)
+            .json(usuarioCriado);
 
-        return usuarioCriado;
+        // return usuarioCriado;
     }
 }
